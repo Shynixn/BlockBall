d1a89b316709a6572e6a797b663cd39a198885b7
Shynixn <shynixn@gmail.com>
2018-03-06 19:52:45 +0100

    Updated documentation page for internet explorer and added favicon.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = "_static/images/BlockBall.png"
+html_logo = "_static/images/BlockBall-small.png"
+html_favicon = "_static/images/favicon.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
