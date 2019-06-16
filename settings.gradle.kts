a70f2aa33a606bca64f86cd53fabc61d0e51c67c
Shynixn <shynixn@gmail.com>
2019-06-16 11:50:49 +0200

    #157 Added hidden entities for 1.12.2.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "blockball-root"
 include("blockball-api")
 include("blockball-bukkit-api")
 include("blockball-bukkit-plugin")
+include("blockball-bukkit-plugin:blockball-bukkit-nms-112R1")
 include("blockball-bukkit-plugin:blockball-bukkit-nms-113R1")
 include("blockball-bukkit-plugin:blockball-bukkit-nms-113R2")
 include("blockball-bukkit-plugin:blockball-bukkit-nms-114R1")
