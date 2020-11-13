8274ce7532e72d82a89e271c7947e75195dab959
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 20:16:15 +0100

    #350 Increased version to snapshot. --snapshot

--- blockball-bukkit-plugin/src/main/java/com/github/shynixn/blockball/bukkit/logic/business/service/ProtocolServiceImpl.kt
+++ blockball-bukkit-plugin/src/main/java/com/github/shynixn/blockball/bukkit/logic/business/service/ProtocolServiceImpl.kt
@@ -78,7 +78,7 @@ class ProtocolServiceImpl @Inject constructor(private val plugin: PluginProxy, p
             try {
                 channel.pipeline().remove(handlerName)
             } catch (e: Exception) {
-                e.printStackTrace()
+                // Ignored
             }
         }
         cachedPlayerChannels.remove(player)
