3efd0fcb0775f1eb14edd1810c5e56e8fca6e20d
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 19:50:10 +0100

    #381 Fixed ball action types. --snapshot

--- blockball-api/src/main/java/com/github/shynixn/blockball/api/business/enumeration/BallActionType.kt
+++ blockball-api/src/main/java/com/github/shynixn/blockball/api/business/enumeration/BallActionType.kt
@@ -1,53 +1,25 @@
 package com.github.shynixn.blockball.api.business.enumeration
 
 /**
- * Created by Shynixn 2018.
- * <p>
- * Version 1.2
- * <p>
- * MIT License
- * <p>
- * Copyright (c) 2018 by Shynixn
- * <p>
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * <p>
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * <p>
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Types of action a ball can execute. Mostly used for events and displaying
+ * effects such as particles and sounds.
  */
 enum class BallActionType {
     /**
      * Ball Pass.
      */
     ONPASS,
+
     /**
      * Ball Kick.
      */
     ONKICK,
+
     /**
      * Ball Touch.
      */
     ONINTERACTION,
-    /**
-     * Ball Throw.
-     */
-    ONTHROW,
-    /**
-     * Ball Grab.
-     */
-    ONGRAB,
+
     /**
      * Ball Spawn.
      */
@@ -61,5 +33,11 @@ enum class BallActionType {
     /**
      * Ball Move.
      */
-    ONMOVE;
+    ONMOVE,
+
+    @Deprecated("No longer being used. Stays for compatibility reason since v6.22.1")
+    ONTHROW,
+
+    @Deprecated("No longer being used. Stays for compatibility reason since v6.22.1")
+    ONGRAB,
 }
