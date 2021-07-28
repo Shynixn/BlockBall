259ea2ea17307cf760180307206911dad8d5eb7e
Shynixn <shynixn@email.com>
2021-07-28 13:20:42 +0200

    #410 Fixed gradle file.

--- blockball-bukkit-plugin/bukkit-nms-117R1/build.gradle.kts
+++ blockball-bukkit-plugin/bukkit-nms-117R1/build.gradle.kts
@@ -2,6 +2,6 @@ dependencies {
     implementation(project(":blockball-api"))
     implementation(project(":blockball-core"))
     compileOnly("com.google.inject:guice:5.0.1")
-    compileOnly("org.spigotmc:spigot:1.17-R0.1-SNAPSHOT:remapped-mojang")
-    testCompile("org.spigotmc:spigot:1.17-R0.1-SNAPSHOT:remapped-mojang")
+    compileOnly("org.spigotmc:spigot:1.17.1-R0.1-SNAPSHOT:remapped-mojang")
+    testCompile("org.spigotmc:spigot:1.17.1-R0.1-SNAPSHOT:remapped-mojang")
 }
