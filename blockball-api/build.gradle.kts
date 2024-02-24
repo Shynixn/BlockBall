61ca96c6ff44d76b9be97adbce8fbef74ac4b271
BuildTools <unconfigured@null.spigotmc.org>
2024-02-24 14:31:07 +0100

    #493 Moved files to simplify structure.

--- blockball-api/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation("com.github.shynixn.mcutils:common:1.0.41")
-}
-
-repositories {
-    maven("https://shynixn.github.io/m2/repository/mcutils")
-}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,81 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.*
 import java.io.*
 
 plugins {
+    id("org.jetbrains.kotlin.jvm") version ("1.6.10")
     id("com.github.johnrengelman.shadow") version ("7.0.0")
 }
 
-tasks.withType<Jar> {
-    archiveName = "${baseName}-${version}-raw.${extension}"
+group = "com.github.shynixn"
+version = "6.40.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi")
+    maven("https://shynixn.github.io/m2/repository/mcutils")
+    maven("https://repo.opencollab.dev/main/")
+}
+
+tasks.register("printVersion") {
+    println(version)
+}
+
+dependencies {
+    // Compile Only
+    compileOnly("me.clip:placeholderapi:2.9.2")
+    compileOnly("net.milkbowlvault:VaultAPI:1.7")
+    compileOnly("org.spigotmc:spigot:1.16.4-R0.1-SNAPSHOT")
+
+    // Library dependencies with legacy compatibility, we can use more up-to-date version in the plugin.yml
+    implementation("com.github.shynixn.org.bstats:bstats-bukkit:1.7")
+    implementation("org.slf4j:slf4j-jdk14:1.7.25")
+    implementation("com.google.inject:guice:5.0.1")
+    implementation("commons-io:commons-io:2.6")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.13.0")
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.3.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.2.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+
+    // Custom dependencies
+    implementation("com.github.shynixn.mcutils:common:1.0.61")
+    implementation("com.github.shynixn.mcutils:packet:1.0.82")
+    implementation("com.github.shynixn.mcutils:sign:1.0.17")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging.showStandardStreams = true
+    failFast = true
+
+    testLogging {
+        events(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED
+        )
+        displayGranularity = 0
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 /**
- * Include all blockball-api, blockball-bukkit-api and exclude debugging classes.
+ * Include all but exclude debugging classes.
  */
-tasks.withType<ShadowJar> {
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     dependsOn("jar")
     archiveName = "${baseName}-${version}-shadowjar.${extension}"
     exclude("DebugProbesKt.bin")
@@ -76,7 +138,6 @@ tasks.register("pluginJarLegacy", ShadowJar::class.java) {
     exclude("plugin-legacy.yml")
 }
 
-
 /**
  * Create legacy plugin jar file.
  */
@@ -106,38 +167,6 @@ tasks.register("pluginJarLatest", ShadowJar::class.java) {
     exclude("com/google/**")
     exclude("com/fasterxml/**")
     exclude("plugin-legacy.yml")
-}
-
-
-repositories {
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi")
-    maven("https://repo.codemc.org/repository/maven-public")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven("https://shynixn.github.io/m2/repository/mcutils")
-}
-
-dependencies {
-    implementation("com.github.shynixn.mcutils:common:1.0.61")
-    implementation("com.github.shynixn.mcutils:packet:1.0.82")
-    implementation("com.github.shynixn.mcutils:sign:1.0.17")
-
-    implementation("com.github.shynixn.org.bstats:bstats-bukkit:1.7")
-    implementation("org.slf4j:slf4j-jdk14:1.7.25")
-    implementation("com.google.inject:guice:5.0.1")
-    implementation("commons-io:commons-io:2.6")
-    implementation("com.google.code.gson:gson:2.8.6")
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.13.0")
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.13.0")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.3.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.2.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-
-    compileOnly("me.clip:placeholderapi:2.9.2")
-    compileOnly("net.milkbowlvault:VaultAPI:1.7")
-    compileOnly("org.spigotmc:spigot:1.16.4-R0.1-SNAPSHOT")
-
-    testImplementation("org.xerial:sqlite-jdbc:3.23.1")
-    testImplementation("org.spigotmc:spigot:1.16.4-R0.1-SNAPSHOT")
 }
 
 tasks.register("languageFile") {
