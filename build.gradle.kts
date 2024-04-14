75bb38a93b5d4e71423584b42dac971f3fd7f375
Shynixn <shynixn@email.com>
2024-04-14 11:50:26 +0200

    #498 Updated to release version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.github.shynixn"
-version = "6.42.0"
+version = "6.42.1"
 
 repositories {
     mavenLocal()
--- src/main/java/com/github/shynixn/blockball/impl/commandexecutor/ReloadCommandExecutor.kt
+++ src/main/java/com/github/shynixn/blockball/impl/commandexecutor/ReloadCommandExecutor.kt
@@ -3,9 +3,11 @@ package com.github.shynixn.blockball.impl.commandexecutor
 import com.github.shynixn.blockball.BlockBallLanguage
 import com.github.shynixn.blockball.contract.CommandExecutor
 import com.github.shynixn.blockball.contract.GameService
+import com.github.shynixn.blockball.entity.Arena
 import com.github.shynixn.mccoroutine.bukkit.launch
 import com.github.shynixn.mcutils.common.ConfigurationService
 import com.github.shynixn.mcutils.common.reloadTranslation
+import com.github.shynixn.mcutils.common.repository.CacheRepository
 import com.google.inject.Inject
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.Plugin
@@ -13,7 +15,8 @@ import org.bukkit.plugin.Plugin
 class ReloadCommandExecutor @Inject constructor(
     private val gameService: GameService,
     private val configurationService: ConfigurationService,
-    private val plugin: Plugin
+    private val plugin: Plugin,
+    private val arenaRepository : CacheRepository<Arena>
 ) : CommandExecutor {
     /**
      * Gets called when the given [source] executes the defined command with the given [args].
@@ -22,6 +25,7 @@ class ReloadCommandExecutor @Inject constructor(
         configurationService.reload()
         val language = configurationService.findValue<String>("language")
         plugin.launch {
+            arenaRepository.clearCache()
             plugin.reloadTranslation(language, BlockBallLanguage::class.java, "en_us")
             gameService.reloadAll()
             source.sendMessage(BlockBallLanguage.reloadMessage)
