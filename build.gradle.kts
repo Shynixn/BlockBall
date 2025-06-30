39d41a3383f3eaa7a6d2526af512a4d950ad0ecb
Shynixn <shynixn@email.com>
2025-06-30 19:27:16 +0200

    #660 Added support for Minecraft 1.21.7.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.shynixn"
-version = "7.22.1"
+version = "7.23.0"
 
 repositories {
     mavenLocal()
@@ -31,10 +31,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 
     // Custom dependencies
-    implementation("com.github.shynixn.shybossbar:shybossbar:1.1.0")
-    implementation("com.github.shynixn.shyscoreboard:shyscoreboard:1.6.0")
-    implementation("com.github.shynixn.mcutils:common:2025.29")
-    implementation("com.github.shynixn.mcutils:packet:2025.26")
+    implementation("com.github.shynixn.shybossbar:shybossbar:1.2.0")
+    implementation("com.github.shynixn.shyscoreboard:shyscoreboard:1.7.0")
+    implementation("com.github.shynixn.mcutils:common:2025.30")
+    implementation("com.github.shynixn.mcutils:packet:2025.29")
     implementation("com.github.shynixn.mcutils:database:2025.7")
     implementation("com.github.shynixn.mcutils:sign:2025.4")
     implementation("com.github.shynixn.mcutils:worldguard:2025.4")
